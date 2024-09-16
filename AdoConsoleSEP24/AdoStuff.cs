524b29fc0afd2d7c2b96c32e7a55f270c0ecc684
Stu Bilyeu <stublue1@outlook.com>
2024-09-15 21:42:48 -0700

    Add project files.

---
+++ AdoConsoleSEP24/AdoStuff.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+//using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoConsoleSEP24
+{
+    internal class AdoStuff
+    {
+        static void HasRows(SqlConnection connection)
+        {
+            using (connection)
+            {
+                //  SELECT CONVERT(char(20), SERVERPROPERTY('productlevel'));  //Determine service pack
+                SqlCommand command = new (
+                    "SELECT CategoryID, CategoryName FROM Categories;", connection );
+                connection.Open ();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if ( reader.HasRows )
+                {
+                    while (reader.Read ())
+                    {
+                        Console.WriteLine ("{0}\t{1}", reader.GetInt32 (0), reader.GetString(1));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                reader.Close ();
+            }
+        }
+    }
+}
