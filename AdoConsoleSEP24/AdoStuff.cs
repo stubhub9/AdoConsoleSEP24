597188ce960e7ee35f862eb6ed14e55cab118e81
Stu Bilyeu <stublue1@outlook.com>
2024-09-16 12:44:45 -0700

    Added a SqlDataReader example method

--- AdoConsoleSEP24/AdoStuff.cs
+++ AdoConsoleSEP24/AdoStuff.cs
@@ -12,6 +12,7 @@ namespace AdoConsoleSEP24
     {
         static void HasRows(SqlConnection connection)
         {
+            #region  Using SqlConnection
             using (connection)
             {
                 //  SELECT CONVERT(char(20), SERVERPROPERTY('productlevel'));  //Determine service pack
@@ -34,6 +35,35 @@ namespace AdoConsoleSEP24
                 }
                 reader.Close ();
             }
+            #endregion  Closed SqlConnection
         }
+
+        static void RetrieveMultipleResults ( SqlConnection connection)
+        {
+            using (connection)
+            {
+                SqlCommand command = new (
+                    "SELECT CategoryID, CategoryName FROM dbo.Categories;" +
+                    "SELECT EmployeeID, LastName FROM dbo.Employees", connection );
+                connection.Open ();
+
+                SqlDataReader reader = command.ExecuteReader ();
+
+                while (reader.HasRows)
+                {
+                    Console.WriteLine("\t{0}\t{1}", reader.GetName(0), reader.GetName(1));
+
+                    while (reader.Read ())
+                    {
+                        Console.WriteLine( "\t{0}\t{1}", reader.GetInt32 ( 0 ), reader.GetString ( 1 ) );
+                    }
+                    reader.NextResult ();
+
+                }
+            }
+            
+        }
+
+
     }
 }
